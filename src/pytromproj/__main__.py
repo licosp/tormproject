fa270a73880ef8d5364cf6dac681b8401215741e
licosp <hastalavista.lk@gmail.com>
2023-09-30 16:13:14 +0900

    Feat: main module of the project

---
+++ src/pytromproj/__main__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""tormproject (TOR Modding Project) start from _main() function."""
+
+
+def main() -> None:
+    raise NotImplementedError
+
+
+if __name__ == "__main__":
+    main()
